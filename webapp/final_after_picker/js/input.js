e20ae17a84914498219eb9fadaaeb84a35c236d0
woolimRyu <directorryu28@gmail.com>
2015-05-13 17:11:34 +0900

    contents input page 1st.

---
+++ webapp/final_after_picker/js/input.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  $('.input--name .input__field').keyup(function() {
+    if ($(this).val()) {
+      $('.input--name').addClass('input--filled');
+    } else {
+      $('.input--name').removeClass('input--filled');
+    }
+  });
+  $('.input--location .input__field').keyup(function() {
+    if ($(this).val()) {
+      $('.input--location').addClass('input--filled');
+    } else {
+      $('.input--location').removeClass('input--filled');
+    }
+  });
+  $('.input--phone .input__field').keyup(function() {
+    if ($(this).val()) {
+      $('.input--phone').addClass('input--filled');
+    } else {
+      $('.input--phone').removeClass('input--filled');
+    }
+  });
+  $('.input--email .input__field').keyup(function() {
+    if ($(this).val()) {
+      $('.input--email').addClass('input--filled');
+    } else {
+      $('.input--email').removeClass('input--filled');
+    }
+  });
+});
