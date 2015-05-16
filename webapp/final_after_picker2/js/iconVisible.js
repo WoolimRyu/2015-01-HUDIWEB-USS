f30a8cc3d2f0742ac0e84fbcc20550fec19564c4
woolimRyu <directorryu28@gmail.com>
2015-05-17 05:00:37 +0900

    woolimRyu

---
+++ webapp/final_after_picker2/js/iconVisible.js
@@ -0,0 +1,47 @@
+$("#i1").keydown (function(){
+    if ($(this).val()) {
+       $('#fb').css('display' , 'block');
+    }
+});
+
+$("#i2").keydown (function(){
+    if ($(this).val()) {
+        $('#tw').css('display' , 'block');
+    }
+});
+
+$("#i3").keydown (function(){
+    if ($(this).val()) {
+        $('#git').css('display' , 'block');
+    }
+});
+
+$("#i4").keydown (function(){
+    if ($(this).val()) {
+        $('#insta').css('display' , 'block');
+    }
+});
+
+$("#i5").keydown (function(){
+    if ($(this).val()) {
+        $('#tr').css('display' , 'block');
+    }
+});
+
+$("#i6").keydown (function(){
+    if ($(this).val()) {
+        $('#vi').css('display' , 'block');
+    }
+});
+
+$("#i7").keydown (function(){
+    if ($(this).val()) {
+        $('#you').css('display' , 'block');
+    }
+});
+
+$("#i8").keydown (function(){
+    if ($(this).val()) {
+        $('#link').css('display' , 'block');
+    }
+});
