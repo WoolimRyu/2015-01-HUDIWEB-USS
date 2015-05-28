c84ab42ba7a5320fc3a549b71c7b1bb1e0c51be6
byjo <buyongjo@gmail.com>
2015-05-28 21:55:31 +0900

    searchController search method 구현. 성호오빠 어서 dao.get 해주시죠

--- src/uss/controller/SearchController.java
+++ src/uss/controller/SearchController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import uss.model.join.UserCard;
 import uss.response.Response;
+import uss.response.Result;
 
 @RestController
 @RequestMapping("/api/search")
@@ -18,7 +20,15 @@ public class SearchController {
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public Response search(String query) {
+		String sql = "SELECT * FROM User JOIN Card "
+				+ "ON User.userId = Card.userId "
+				+ "WHERE User.name LIKE '%query%' OR User.email LIKE '%query%' "
+				+ "OR Card.phoneNumber LIKE '%query%' OR Card.company LIKE '%query%'";
 		
-		return null;
+		sql = sql.replaceAll("query", query);
+		UserCard userCard = dao.get(UserCard.class, sql);
+		if (userCard == null)
+			return Result.getErrorSearchNotFound();
+		return Result.getSuccess(userCard);
 	}
 }
