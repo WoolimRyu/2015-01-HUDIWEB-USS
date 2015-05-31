a9f83c3249f9db419434ed0e800d772be028b406
woolimRyu <directorryu28@gmail.com>
2015-06-01 05:39:39 +0900

    asdfghv

--- webapp/phase2_newStyle/js/input.js
+++ webapp/phase2_newStyle/js/input.js
@@ -27,4 +27,11 @@ $(document).ready(function() {
       $('.input--email').removeClass('input--filled');
     }
   });
+  $('.input--address .input__field').keyup(function() {
+    if ($(this).val()) {
+      $('.input--address').addClass('input--filled');
+    } else {
+      $('.input--address').removeClass('input--filled');
+    }
+  });
 });
