a1390e89b4a82df09330b42e5e7358a0632a8628
zerohouse <parksungho86@gmail.com>
2015-06-01 00:59:49 +0900

    v

--- src/uss/util/SessionUtil.java
+++ src/uss/util/SessionUtil.java
@@ -11,8 +11,6 @@ public class SessionUtil {
 
 	public static User getUser(HttpSession session) throws SessionNullException {
 		User user = (User) session.getAttribute(USER);
-		if (user == null)
-			throw new SessionNullException();
 		return user;
 	}
 
